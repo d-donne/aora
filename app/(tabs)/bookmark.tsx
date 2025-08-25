a0983d85a3bb0fb39b54f2248be1dbf8dfa9aec4
d-donne <doublyd@gmail.com>
2025-08-25 21:50:41 +0000

    refactor: add TestComponent for video playback

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import TestComponent from '@/components/TestComponent'
 
 const Bookmark = () => {
   return (
-    <View>
+    <View className='flex-1 justify-center items-center bg-primary'>
       <Text>Bookmark</Text>
+      {/* <TestComponent /> */}
     </View>
   )
 }
---
+++ components/TestComponent.tsx
@@ -0,0 +1,57 @@
+import useAppWrite from '@/hooks/useAppWrite';
+import { getLatestPosts } from '@/lib/appwrite';
+import { useEvent } from 'expo';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { StyleSheet, View, Button } from 'react-native';
+
+const videoSource =
+  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+
+export default function TestComponent() {
+    const {data} = useAppWrite(getLatestPosts);
+    // console.log(data[0].video);
+    // const source = { uri: data[0].video };
+    const source = 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4'
+
+  const player = useVideoPlayer(source, player => {
+    player.loop = true;
+    player.play();
+  });
+
+  const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+
+  return (
+    <View style={styles.contentContainer}>
+      <VideoView style={styles.video} player={player} allowsFullscreen allowsPictureInPicture />
+      <View style={styles.controlsContainer}>
+        <Button
+          title={isPlaying ? 'Pause' : 'Play'}
+          onPress={() => {
+            if (isPlaying) {
+              player.pause();
+            } else {
+              player.play();
+            }
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 50,
+  },
+  video: {
+    width: 350,
+    height: 275,
+  },
+  controlsContainer: {
+    padding: 10,
+  },
+});
